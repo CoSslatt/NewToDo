ca409cc7b438639a4760cc902c756692a90c7ad2
narcikss <mdgamedeveloper@gmail.com@gmail.com>
2022-04-12 18:51:08 +0200

    base

---
+++ main.js
@@ -0,0 +1,41 @@
+const userContent = document.querySelector('#user-content');
+
+let currentInput;
+let previousInput;
+let inputExists = false;
+
+window.addEventListener('click', e => {
+
+    if (e.target.innerText != 'add') return;
+    if (inputExists){inputAlreadyExists(); return;}
+
+    const input = document.createElement('input');
+    const newInput = userContent.appendChild(input);
+    newInput.classList.add('task-input');
+
+    currentInput = newInput;
+    inputExists = true;
+});
+
+window.addEventListener('keypress', e => {
+    if (e.key != 'Enter') return;
+
+    typeTask(currentInput);
+});
+
+const typeTask = (input) => {
+    userContent.removeChild(input);
+
+    const div = document.createElement('div');
+
+    const newDiv = userContent.appendChild(div);
+    newDiv.classList.add('task');
+    newDiv.innerText = input.value;
+
+    inputExists = false;
+};
+
+const inputAlreadyExists = () => {
+    currentInput.style.boxShadow = "0 0 10px red";
+    currentInput.style.border = "1px solid red";
+};
